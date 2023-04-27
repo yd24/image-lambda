6b6c6439d0821e2e8dc8bfe99bf245f272bf2f87
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2023-04-27 10:21:20 -0700

    Implement lambda function and related unit tests

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # image-lambda
 
+## Description
+
+Code that can be placed into an AWS Lambda function. This Lambda code will keep track of an images/ folder on an S3 Bucket and update a file called images.json everytime an image is uploaded to the folder.
+
+## Setup
+
+* First, create an AWS account.
+* Create an S3 Bucket with permissions configured to allow for read and writes.
+* Create an images/ folder in the S3 Bucket.
+* Create a Lambda function and insert the code snippet. Ensure that requires are replaced with imports (due to Amazon using .mjs files).
+* Deploy your Lambda function.
+* Setup a trigger for your S3 Bucket to the Lambda function, set for every object creation in the images/ folder of type .jpg.
+
+## Some issues that I ran across
+
+* AWS GetObjectCommand and PutObjectCommand documentation didn't have examples for reading file data from Body.
+* In order to read JSON, we need to use transformToString() and an await on the response.Body from GetObjectCommand.
+* For setting up permissions on S3 Bucket, if we use an object policy then Version does not refer to your own "version", but a set of pre-defined versions from AWS. You'll have to look these up online.
--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 'use strict';
+
+const {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+} = require('@aws-sdk/client-s3');
+
+const s3Client = new S3Client({ region: 'us-west-2' });
+
+module.exports = async(event) => {
+  const bucketName = event.Records[0].s3.bucket.name;
+  const fileName = event.Records[0].s3.object.key;
+  const fileSize = event.Records[0].s3.object.size;
+  console.log(`${bucketName} ${fileName} ${fileSize}`);
+
+  let img = {
+    name: fileName,
+    size: fileSize,
+    type: 'image',
+  };
+
+  const response = {
+      statusCode: 200,
+      body: JSON.stringify('Hello from Lambda!'),
+  };
+  
+  let getImageManifest = {
+      Bucket: bucketName,
+      Key: 'images.json',
+  };
+
+  let imgArr = [];
+  try {
+      const manifest = await s3Client.send(new GetObjectCommand(getImageManifest));
+      let body = await manifest.Body.transformToString();
+      imgArr = JSON.parse(body);
+  } catch (e) {
+      if (e.Code === 'NoSuchKey') {
+        console.log(e);
+      }
+  }
+  let index = imgArr.findIndex((element) => element.name === fileName);
+  if (index > -1) {
+    imgArr[index] = img;
+  } else {
+    imgArr.push(img);
+  }
+  getImageManifest = {
+    Bucket: bucketName,
+    Key: 'images.json',
+    Body: Buffer.from(JSON.stringify(imgArr)),
+  };
+  await s3Client.send(new PutObjectCommand(getImageManifest));
+  return response;
+};
